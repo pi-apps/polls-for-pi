83d695ed50be2cce46671a21eca56060a72404e1
East Mael <eastmaels@gmail.com>
2023-03-19 21:14:53 +0900

    remove random imports

--- backend/src/handlers/public_polls.ts
+++ backend/src/handlers/public_polls.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
-import _ from 'lodash';
-import { v4 as uuidv4 } from 'uuid';
 import "../types/session";
-import { getPollEndDate, getPollResponseEndDate } from "../utils/poll_utils";
 
 export default function mountPublicPollsEndpoints(router: Router, models: any) {
 
   router.get('/', async (req, res) => {
     console.log('get public polls')
     const { Poll } = models;
-    const { filter, responseUrl, username } = req.query;
 
     let items = await Poll.aggregate([
       {
